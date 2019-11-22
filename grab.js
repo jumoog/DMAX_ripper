fc4738811d66bf44d55412ede190cd2704096aa3
Kilian von Pflugk <github@jumoog.io>
2019-11-22 19:00:14 +0100

    make grabber dynamic

--- grab.js
+++ grab.js
@@ -1,13 +1,21 @@
 const URL = "https://www.dmax.de/programme/steel-buddies/";
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(URL);
     const text = await page.evaluate(id => window.__APP_INITIAL_STATE__);
-    text["/api/show-detail/steel-buddies"].videos.episode[3].forEach(function (name) {
-        console.log(name.path);
-    });
+    let episodes = text["/api/show-detail/steel-buddies"].videos.episode;
+    fs.writeFile('dump.sh', "#!/bin/bash\n", { flag: 'a+' }, (err) => {});
+    for (var key in episodes)
+    {
+        fs.writeFile('dump.sh', `echo "Season <${key}>"\n`, { flag: 'a+' }, (err) => {});
+        text["/api/show-detail/steel-buddies"].videos.episode[key].forEach(function (name) {
+            fs.writeFile('dump.sh', `node loader.js "https://www.dmax.de/programme/steel-buddies/video/${name.path.split("/")[1]}/"\n`, { flag: 'a+' }, (err) => {});
+            console.log(`https://www.dmax.de/programme/steel-buddies/video/${name.path.split("/")[1]}/`);
+        });
+    }
     await browser.close();
 })();
