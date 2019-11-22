168d0472006837d01abcc684215ffc54cc0ca3de
Kilian von Pflugk <github@jumoog.io>
2019-11-22 21:10:42 +0100

    dynamic grab

--- grab.js
+++ grab.js
@@ -1,4 +1,11 @@
-const URL = "https://www.dmax.de/programme/steel-buddies/";
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " DMAX programme URL");
+    console.log("for example: " + __filename + "https://www.dmax.de/programme/steel-buddies/");
+    process.exit(-1);
+}
+
+const URL = process.argv[2];
+const show = URL.split('/').pop() || URL.split('/').pop();
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
@@ -11,14 +18,15 @@ const fs = require('fs');
     const page = await browser.newPage();
     await page.goto(URL);
     const text = await page.evaluate(id => window.__APP_INITIAL_STATE__);
-    let episodes = text["/api/show-detail/steel-buddies"].videos.episode;
+    let api_show_key = "/api/show-detail/"+show;
+    let episodes = text[api_show_key].videos.episode;
     fs.writeFile('dump.sh', "#!/bin/bash\n", { flag: 'a+' }, (err) => {});
     for (var key in episodes)
     {
         fs.writeFile('dump.sh', `echo "Season <${key}>"\n`, { flag: 'a+' }, (err) => {});
-        text["/api/show-detail/steel-buddies"].videos.episode[key].forEach(function (name) {
-            fs.writeFile('dump.sh', `node loader.js "https://www.dmax.de/programme/steel-buddies/video/${name.path.split("/")[1]}/"\n`, { flag: 'a+' }, (err) => {});
-            console.log(`https://www.dmax.de/programme/steel-buddies/video/${name.path.split("/")[1]}/`);
+        text[api_show_key].videos.episode[key].forEach(function (name) {
+            fs.writeFile('dump.sh', `node loader.js "https://www.dmax.de/programme/${show}/video/${name.path.split("/")[1]}/"\n`, { flag: 'a+' }, (err) => {});
+            console.log(`https://www.dmax.de/programme/${show}/video/${name.path.split("/")[1]}/`);
         });
     }
     await browser.close();
