f6773018667ec1a1b3e041b18dacc03ad89cb838
Kilian von Pflugk <github@jumoog.io>
2019-11-23 13:16:35 +0100

    add URL check.. output real episode names

--- grab.js
+++ grab.js
@@ -8,7 +8,7 @@ const URL = process.argv[2];
 const show = URL.split('/').pop() || URL.split('/').pop();
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-
+let episodes;
 (async () => {
     const browser = await puppeteer.launch(
         {
@@ -19,14 +19,22 @@ const fs = require('fs');
     await page.goto(URL);
     const text = await page.evaluate(id => window.__APP_INITIAL_STATE__);
     let api_show_key = "/api/show-detail/"+show;
-    let episodes = text[api_show_key].videos.episode;
+    try {
+        episodes = text[api_show_key].videos.episode;
+    } catch (e) {
+        console.log(`${URL} is not valid`);
+        await browser.close();
+        process.exit(-1);
+    }
+
     fs.writeFile('dump.sh', "#!/bin/bash\n", { flag: 'a+' }, (err) => {});
     for (var key in episodes)
     {
+        console.log(`Season <${key}>`);
         fs.writeFile('dump.sh', `echo "Season <${key}>"\n`, { flag: 'a+' }, (err) => {});
         text[api_show_key].videos.episode[key].forEach(function (name) {
             fs.writeFile('dump.sh', `node loader.js "https://www.dmax.de/programme/${show}/video/${name.path.split("/")[1]}/"\n`, { flag: 'a+' }, (err) => {});
-            console.log(`https://www.dmax.de/programme/${show}/video/${name.path.split("/")[1]}/`);
+            console.log(`S${name.season}.E${name.episode} ${name.name}`);
         });
     }
     await browser.close();
